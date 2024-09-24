98136820b9502d76863aae1869f8f36cad446053
pierrialexandervidmar <pierri@hotmail.com.br>
2024-09-23 22:35:39 -0300

    Ajustes iniciais de Controller e View

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -1,14 +1,20 @@
 <?php
 
-namespace App\Http\Controllers\Site;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AdminHomeController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() 
     {
-
+        return view("admin.home");
     }
 }
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,1 @@
+<h1>Home do Admin</h1>
